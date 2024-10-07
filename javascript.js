fcc5765ae262c23f824d514da2c8de00ecfcd2a8
Nickname147 <antza555@gmail.com>
2024-10-07 15:46:27 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-
+document.getElementById('upload-btn').addEventListener('click', () => {
+    const fileInput = document.getElementById('screenshot');
+    if (fileInput.files.length === 0) {
+        alert('Please select an image to upload.');
+    } else {
+        alert('Image uploaded: ' + fileInput.files[0].name);
+        // Here you can add the functionality to send the image to the server
+    }
+});
